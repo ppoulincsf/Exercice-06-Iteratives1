7a0753ebba690c29bebf59569f42908e105c7a24
Pierre Poulin <ppoulin@cegep-ste-foy.qc.ca>
2023-01-05 10:40:13 -0500

    Ajout exercice sur les séquences

---
+++ Exercice-03-Sequences/exercice-03/Exercice03/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Exercice03
+{
+  public class Program
+  {
+    public const float PI = 3.14159265359f;
+    public const float REBATE_RATE = 0.10f;
+    public const float TPS_RATE = 0.05f;
+    public const float TVQ_RATE = 0.09975f;
+
+    public static void Main(string[] args)
+    {
+      CirclePerimeter();          // Excution de la solution du problème #1
+      NumbersAverage();           // Excution de la solution du problème #2
+      ApplyRebate();              // Excution de la solution du problème #3
+      SwapNumbers();              // Excution de la solution du problème #4
+      ComputePriceWithTaxes();    // Excution de la solution du problème #5
+    }
+
+    public static void CirclePerimeter()
+    {
+      // Entrées
+      float radius = 5.5f;
+
+      //Algorithme
+      //Écrivez ici l'algorithme du problème #1
+      
+
+      // Sortie
+      
+    } 
+
+    public static void NumbersAverage()
+    {
+      // Entrées
+      int number1 = 75;
+      int number2 = 88;
+      int number3 = 53;
+
+      // Algorithme
+      //Écrivez ici l'algorithme du problème #2
+
+      // Sortie
+      
+    }
+
+    public static void ApplyRebate()
+    {
+      // Entrées
+      float price = 75.0f;
+
+      // Algorithme
+      //Écrivez ici l'algorithme du problème #3
+
+      // Sortie
+      
+    }
+
+    public static void SwapNumbers()
+    {
+      // Entrées
+      int number1 = 1;
+      int number2 = 2;
+
+      // Algorithme
+      //Écrivez ici l'algorithme du problème #4
+
+      // Sortie
+      
+    }
+
+    public static void ComputePriceWithTaxes()
+    {
+      // Entrées
+      float price = 100.0f;
+
+      // Algorithme
+      //Écrivez ici l'algorithme du problème #5
+
+      // Sortie
+      
+    }
+  }
+}
