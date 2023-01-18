039b2afac2426407565f2e7127313acc1848a5aa
Pierre Poulin <ppoulin@cegep-ste-foy.qc.ca>
2023-01-18 09:38:56 -0500

    Initial commit

---
+++ Exercice-06-Iteratives1/Exercice06/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Exercice06
+{
+  public class Program
+  {
+    public static void Main ( string[] args )
+    {
+      PrintNumbers ();                   // Question 1
+      ComputeSum ();                     // Question 2
+      ComputeProductOfEvenNumbers ();    // Question 3
+      ComputeSumOfDigits ();             // Question 4
+      GreatestCommonDivisor ();          // Question 5 [Bonus]
+    }
+
+    public static void PrintNumbers ()
+    {
+      // Entrées
+      int numberMin = 3;
+      int numberMax = 9;
+
+      // A COMPLETER
+    }
+
+    public static void ComputeSum ()
+    {
+      //Entrées
+      int limitNumber = 5;
+      
+      // A COMPLETER
+    }
+
+    public static void ComputeProductOfEvenNumbers ()
+    {
+      int limiteNumber = 6;
+	  
+      // A COMPLETER
+    }
+	
+    public static void ComputeSumOfDigits ()
+    {
+      // Entrées
+      int number = 15;
+
+      // A COMPLETER
+    }
+	
+	public static void GreatestCommonDivisor ()
+    {
+      // Entrées
+      int a = 27;
+	  int b = 12;
+
+      // A COMPLETER
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void GreatestCommonDivisor ()
+    {
+      // Entrées
+      int a = 12;
+      int b = 21;
+
+      // Traitements
+      int pgcd = a;
+      while (b != 0)
+      {
+        int remaining = a % b;
+        a = b;
+        b = remaining;
+      }
+
+      // Sorties
+      pgcd = a;
+    }
+  }
+}
